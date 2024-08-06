415358b4ee1541f5cb298b8dc8f5768258be4ae6
penn <penn201500@gmail.com>
2024-08-07 03:16:21 +0800

    array.pop()

--- array.js
+++ array.js
@@ -1,7 +1,10 @@
 // push
-let array = [1,2]
-array.push('A', 'B')
-console.log(`Here is the array: ${array}`);
-console.log(`type of array: ${Object.prototype.toString.call(array)}`);
+let array = [1, 2]
+array.push("A", "B")
+console.log(`Here is the array: ${array}`)
+console.log(`type of array: ${Object.prototype.toString.call(array)}`)
 
-
+// pop
+// pop will remove the last element of the array. And return the removed element.
+console.log(array.pop())
+console.log(array)
