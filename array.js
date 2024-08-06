e8114e9f817b6bed6e91fa920a8a95cf49a1f51f
penn <penn201500@gmail.com>
2024-08-07 03:26:27 +0800

    shift and unshift of an array

--- array.js
+++ array.js
@@ -16,3 +16,16 @@ console.log(array.pop()) // this one is undefined
 console.log(array) // the array is empty now
 console.log(array.pop()) // this one is undefined
 console.log(array) // the array is still empty
+
+// unshift
+let array2 = [1, 2]
+console.log(array2.unshift("A", "B")) // return the length of the array, 4
+console.log(array2) // ['A', 'B', 1, 2]
+console.log(array2.shift()) // 'A'
+console.log(array2) // ['B', 1, 2]
+console.log(array2.shift()) // 'B'
+console.log(array2.shift()) // 1
+console.log(array2.shift()) // 2
+console.log(array2) // []
+console.log(array2.shift()) // shift an empty array will return undefined
+console.log(array2) // []
