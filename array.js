b6561b0269ff52c2bc3fd7068285f02f008374ee
penn <penn201500@gmail.com>
2024-08-07 03:20:33 +0800

    if array is empty, array.pop() will return undefined

--- array.js
+++ array.js
@@ -8,3 +8,11 @@ console.log(`type of array: ${Object.prototype.toString.call(array)}`)
 // pop will remove the last element of the array. And return the removed element.
 console.log(array.pop())
 console.log(array)
+
+console.log(array.pop()) // this one is 'A'
+console.log(array.pop()) // this one is 2
+console.log(array.pop()) // this one is 1
+console.log(array.pop()) // this one is undefined
+console.log(array) // the array is empty now
+console.log(array.pop()) // this one is undefined
+console.log(array) // the array is still empty
