6f836c62191c7eeac765da11ccd630354bef87e6
penn <penn201500@gmail.com>
2024-08-12 01:37:29 +0800

    split() method of string

---
+++ string.js
@@ -0,0 +1,12 @@
+// charAt
+
+// concat
+
+// split
+const str = "Hello World One Two Three"
+console.log(str.split(" ")) // ["Hello", "World", "One", "Two", "Three"]
+console.log(str.split("")) // ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", " ", "O", "n", "e", " ", "T", "w", "o", " ", "T", "h", "r", "e", "e"]
+console.log(str.split(" ", 2)) // ["Hello", "World"]
+console.log(str.split()) // ["Hello World One Two Three"]
+const str2 = " Hello World "
+console.log(str2.split(" ")) // ["", "Hello", "World", ""]
