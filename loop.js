bb93db8a63c49aff3fa435d779be05a2f45e2b9a
penn <penn201500@gmail.com>
2024-08-12 12:52:35 +0800

    ways to loop through index:value in array

--- loop.js
+++ loop.js
@@ -23,3 +23,28 @@ Object.entries(obj).forEach(([key, value]) => {
 for (let [key, value] of Object.entries(obj)) {
     console.log(`${key}: ${value}`)
 }
+
+// loop through array
+const arr = ["Alice", 25, "New York"]
+// use for loop
+for (let i = 0; i < arr.length; i++) {
+    console.log(`${i}: ${arr[i]}`)
+}
+// use for..of and entries()
+for (let [index, value] of arr.entries()) {
+    console.log(`${index}: ${value}`)
+}
+// use map() method
+arr.map((value, index) => console.log(`${index}: ${value}`))
+// use filter() method
+arr.filter((value, index) => {
+    if (typeof value === "string") {
+        console.log(`${index}: ${value}`)
+    }
+})
+// use reduce() method
+const acc = arr.reduce((acc, value, index) => {
+    console.log(`${index}: ${value}`)
+    return acc
+}, 0)
+console.log(acc)
