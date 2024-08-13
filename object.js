c6d46a4132425c763d0d5b074a4cb3dc811b5a73
penn <penn201500@gmail.com>
2024-08-13 11:01:00 +0800

    checking if a property exists in an object

--- object.js
+++ object.js
@@ -37,3 +37,29 @@ console.log(state) // NY
 const { name: name3, ...rest } = obj
 console.log(name3) // Alice
 console.log(rest) // { age: 25, city: "New York" }
+
+// checking if a property exists in an object
+// hasOwnProperty()
+const obj2 = {
+    number: 1,
+    height: 2,
+    length: 3,
+}
+console.log(obj2.hasOwnProperty("number")) // true
+console.log(obj2.hasOwnProperty("width")) // false
+// using 'in' operator
+console.log("number" in obj2) // true
+console.log("width" in obj2) // false
+// using Object.keys()
+console.log(Object.keys(obj2).includes("number")) // true
+console.log(Object.keys(obj2).includes("width")) // false
+// about the 'in' operator: It checks if a property exists either directly on the object or anywhere in its prototype chain.
+const animal = {
+    eats: true,
+}
+const obj3 = Object.create(animal)
+obj3.run = true
+console.log("run" in obj3) // true
+console.log("eats" in obj3) // true
+console.log(obj3.hasOwnProperty("run")) // true
+console.log(obj3.hasOwnProperty("eats")) // false
