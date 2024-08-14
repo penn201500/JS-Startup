8ea93196e0efa0f486202c4c22c1d807fe1645e8
penn <penn201500@gmail.com>
2024-08-14 15:03:21 +0800

    some common methods of string

--- string.js
+++ string.js
@@ -5,7 +5,27 @@ console.log(str3.charAt(0)) // H
 // length
 console.log(str3.length) // 11
 
+// toUpperCase
+console.log(str3.toUpperCase()) // HELLO WORLD
+// toLowerCase
+console.log(str3.toLowerCase()) // hello world
+// substring
+console.log(str3.substring(0, 5)) // Hello
+// slice
+console.log(str3.slice(0, 5)) // Hello
+console.log(str3)
+// indexOf
+console.log(str3.indexOf("o")) // 4
+// includes
+console.log(str3.includes("o")) // true
+// replace
+console.log(str3.replace("World", "Alice")) // Hello Alice
+// trim : remove whitespace from both ends of the string
+console.log(str3.trim()) // Hello World
 // concat
+const str4 = "hello"
+const str5 = "world"
+console.log(str4.concat(" ", str5, "!")) // hello world!
 
 // split
 const str = "Hello World One Two Three"
