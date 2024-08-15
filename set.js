82d20015c953772af90da44147350e69a4477826
penn <penn201500@gmail.com>
2024-08-15 11:26:44 +0800

    has() and delete() method of Set object

--- set.js
+++ set.js
@@ -6,3 +6,10 @@ set.add(1)
 console.log(set) // Set {0: 'a', 1: 'b', 2: 1}
 set.add("a")
 console.log(set) // Set {0: 'a', 1: 'b', 2: 1}  add duplicate value will be ignored
+// has(): checks if a value is in the set
+console.log(set.has("a")) // true
+console.log(set.has("c")) // false
+// delete(): removes a value from the set
+set.delete("a")
+console.log(set) // Set {0: 'b', 1: 1}
+
